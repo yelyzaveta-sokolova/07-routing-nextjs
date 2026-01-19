e3b11f5bde28501399b556def464c8fe6242f2e0
yelyzaveta-sokolova <sokolova.yelyzavetaaa@gmail.com>
2026-01-19 14:03:47 +0100

    1.3

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,14 +1,7 @@
 export default function NotesLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode
-  modal: React.ReactNode
 }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  )
+  return <>{children}</>
 }
